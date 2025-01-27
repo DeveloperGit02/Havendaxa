aa6ab7364848d5564a24ce67ee56f226dde110be
Vijay Sharma <113002348+VijaySharma898@users.noreply.github.com>
2025-01-27 18:50:13 +0530

    commit

--- Havendaxa/Havendaxa/Dashboard/Controller/SinglePropertiesVC.swift
+++ Havendaxa/Havendaxa/Dashboard/Controller/SinglePropertiesVC.swift
@@ -7,16 +7,20 @@
 
 import UIKit
 
-class SinglePropertiesVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+class SinglePropertiesVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 6
+        return 2
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! PropertiesCollectionViewCell
         return cell
     }
-    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            // Set cell size to match the collection view's visible size
+        return CGSize(width: collectionView.bounds.width, height: collectionView.bounds.width)
+        }
+
     
     @IBOutlet weak var collectionView: UICollectionView!
     
